ead3880dd68a8029839d7fe92378fd0a9265bfcf
ejolikestocode <90877524+ejolikestocode@users.noreply.github.com>
2022-04-25 20:14:00 -0400

    Add files via upload

---
+++ street-fighter-2/js/main.js
@@ -0,0 +1,72 @@
+document.getElementById('ryu').onclick = RyuStage
+document.getElementById('e-honda').onclick = eHondaStage
+document.getElementById('blanka').onclick = blankaStage
+document.getElementById('guile').onclick = guileStage
+document.getElementById('balrog').onclick = balrogStage
+document.getElementById('vega').onclick = vegaStage
+document.getElementById('ken').onclick = kenStage
+document.getElementById('chun-li').onclick = chunliStage
+document.getElementById('zangief').onclick = zangiefStage
+document.getElementById('dhalsim').onclick = dhalsimStage
+document.getElementById('sagat').onclick = sagatStage
+document.getElementById('m-bison').onclick = mbisonStage
+
+function RyuStage() {
+  document.querySelector('body').style.background = "no-repeat center / cover url('img/ryu-stage.gif')"
+  document.querySelector('body').style.color = 'white'
+}
+
+function eHondaStage() {
+  document.querySelector('body').style.background = "no-repeat center / cover url('img/ehonda-stage.gif')"
+  document.querySelector('body').style.color = 'white'
+}
+
+function blankaStage() {
+  document.querySelector('body').style.background = "no-repeat center / cover url('img/blanka-stage.gif')"
+  document.querySelector('body').style.color = 'white'
+}
+
+function guileStage() {
+  document.querySelector('body').style.background = "no-repeat center / cover url('img/guile-stage.gif')"
+  document.querySelector('body').style.color = 'white'
+}
+
+function balrogStage() {
+  document.querySelector('body').style.background = "no-repeat center / cover url('img/balrog-stage.gif')"
+  document.querySelector('body').style.color = 'white'
+}
+
+function vegaStage() {
+  document.querySelector('body').style.background = "no-repeat center / cover url('img/vega-stage.gif')"
+  document.querySelector('body').style.color = 'white'
+}
+
+function kenStage() {
+  document.querySelector('body').style.background = "no-repeat center / cover url('img/ken-stage.gif')"
+  document.querySelector('body').style.color = 'white'
+}
+
+function chunliStage() {
+  document.querySelector('body').style.background = "no-repeat center / cover url('img/chunli-stage.gif')"
+  document.querySelector('body').style.color = 'white'
+}
+
+function zangiefStage() {
+  document.querySelector('body').style.background = "no-repeat center / cover url('img/zangief-stage.gif')"
+  document.querySelector('body').style.color = 'white'
+}
+
+function dhalsimStage() {
+  document.querySelector('body').style.background = "no-repeat center / cover url('img/dhalsim-stage.gif')"
+  document.querySelector('body').style.color = 'white'
+}
+
+function sagatStage() {
+  document.querySelector('body').style.background = "no-repeat center / cover url('img/sagat-stage.gif')"
+  document.querySelector('body').style.color = 'white'
+}
+
+function mbisonStage() {
+  document.querySelector('body').style.background = "no-repeat center / cover url('img/mbison-stage.gif')"
+  document.querySelector('body').style.color = 'white'
+}
